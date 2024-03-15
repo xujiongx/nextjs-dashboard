535bd101303f97d9f0ab9ef7ccc75466ff715174
xuj16 <xuj16@mingyuanyun.com>
2024-03-15 11:50:48 +0800

    pref: customer

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return <CustomersTable customers={customers} />;
 }
